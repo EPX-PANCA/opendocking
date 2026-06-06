dd019ab7ef08dd87f6204db43e5096a556f27885
Panca Putra Pahlawan <pancaputrapahlawan@Pancas-MacBook-Pro.local>
2026-06-07 00:25:40 +0700

    Fix: remove Documentation link from sidebar

--- app/react/sidebar/SettingsSidebar.tsx
+++ app/react/sidebar/SettingsSidebar.tsx
@@ -191,16 +191,6 @@ export function SettingsSidebar({
             data-cy="portainerSidebar-edgeCompute"
           />
 
-          <SidebarItem.Wrapper label="Documentation">
-            <a
-              href="https://opendocking.io/docs"
-              target="_blank"
-              rel="noreferrer"
-              className="flex h-8 w-full items-center rounded px-3 text-sm !text-inherit transition-colors duration-200 hover:bg-blue-5/20 hover:!underline focus:no-underline be:hover:bg-gray-5/20 th-dark:hover:bg-gray-true-5/20"
-            >
-              Documentation
-            </a>
-          </SidebarItem.Wrapper>
         </SidebarParent>
       )}
     </SidebarSection>
